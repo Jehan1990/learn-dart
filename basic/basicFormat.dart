47e3f12a4f9a998a9ebe8a0316536386fbfb2d23
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-11 09:23:03 +0000

    add

--- basic/basicFormat.dart
+++ basic/basicFormat.dart
@@ -1,6 +1,11 @@
 void main() {
-  print('I Love Zia');
+  print('Hello Dart World');
 }
 /*
-void = means d
+void = means the main function does not return anything
+main()= Entry point fuction of a dart program. Every dart 
+        will have a 1 main() function to start the program
+print()=is an in built dart function to print things on the console.
+{}=open and close parenthese are used to define the body
+of the function main()
 */
---
+++ basic/variables.dart
@@ -0,0 +1,27 @@
+//Variables 
+
+/*- Variable store value(literal)
+  - Variable declaration and assignment
+
+  - String fullName = 'Jehan Ichhaporia'
+  
+  - String=Data type of Variable
+  - fullname=name of the variable
+  - 'Jehan Ichhaporia'=Value stored in variable(also called 
+    string literal)
+
+ r6jmm.com.com 4 basic type of variable:-String,int,double,bool
+*/
+
+void main() {
+  String name = 'Jehan Ichhapporia';//String literal
+  int age = 45;//int literal
+  double height = 5.7;//double literal
+  bool married = true;//bool literal
+
+  print('Full Name = $name');//$ is used to access the value of variable
+  print('Age = $age');
+  print('Height = $height');
+  print('Marrital Status = $married');
+
+}

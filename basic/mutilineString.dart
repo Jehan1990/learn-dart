784ae6aec7e8646f75afd8c24c0fe551bfbd2c82
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-14 10:00:23 +0000

    add

---
+++ basic/mutilineString.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  //Multiline string using triple single quotes
+  String s = '''1-Short string
+2-Long string
+3-String with multiple lines
+4-String with multiple lines''';
+
+//Multiline string using triple single quotes on diffrent lines
+String s1 = '''
+1-Short string
+2-Long string
+3-String with multiple lines
+4-String with multiple lines
+''';
+  print(s+'\n');
+  print(s1);
+}

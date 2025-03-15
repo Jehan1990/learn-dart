ed841d8df0a94aa47f4e4fb281b5e2873d5a41da
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-15 15:47:40 +0000

    add

---
+++ basic/stringmethods.dart
@@ -0,0 +1,12 @@
+void main() {
+  String text = 'Test String'; //initialization
+  print(text.toUpperCase());
+  print(text.toLowerCase());
+  print(text.length);
+
+  String text2 = 'Test String 2'.toUpperCase(); //initialization
+  print(text2);
+  text2 = text2.toLowerCase(); //reassigning
+  print(text2);
+  print(text2.length);
+}

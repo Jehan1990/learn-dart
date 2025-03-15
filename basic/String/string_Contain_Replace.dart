2fe68f2176c3b32aee64db6918e923b0d5bee8e4
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-15 17:40:05 +0000

    add

--- basic/String/string_Contain_Replace.dart
+++ basic/String/string_Contain_Replace.dart
@@ -8,4 +8,8 @@ void main() {
 
   //replace method
   print(foodPreference.replaceAll('pizza', 'pasta'));
+
+  //indexOf method
+  int i=foodPreference.indexOf('love');
+  print(i+1);//index start from 0
 }

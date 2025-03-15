07839f0e9f7192289dec9732e83f32836e112289
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-15 17:35:47 +0000

    add

---
+++ basic/String/string_Contain_Replace.dart
@@ -0,0 +1,11 @@
+void main() {
+  String foodPreference = 'I love pizza';
+
+  print(foodPreference);
+
+  //contains method
+  print('does it contain pizza: ${foodPreference.contains('pizza')}');
+
+  //replace method
+  print(foodPreference.replaceAll('pizza', 'pasta'));
+}

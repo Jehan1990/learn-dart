524afdcd86c226648ae3bdab2fa9cdc206d11227
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-16 08:55:49 +0000

    add

--- basic/variables.dart
+++ basic/variables.dart
@@ -1,4 +1,4 @@
-//Variables 
+//Variables
 
 /*- Variable store value(literal)
   - Variable declaration and assignment
@@ -13,13 +13,15 @@
  r6jmm.com.com 4 basic type of variable:-String,int,double,bool
 */
 
-void main() {
-  String name = 'Jehan Ichhapporia';//String literal
-  int age = 45;//int literal
-  double height = 5.7;//double literal
-  bool married = true;//bool literal
+import 'dart:typed_data';
 
-  print('Full Name = $name');//$ is used to access the value of variable
+void main() {
+  String name = 'Jehan Ichhapporia'; //String literal
+  int age = 45; //int literal
+  double height = 5.7; //double literal
+  bool married = true; //bool literal
+
+  print('Full Name = $name'); //$ is used to access the value of variable
   print('Age = $age');
   print('Height = $height');
   print('Marrital Status = $married');
@@ -28,5 +30,21 @@ void main() {
   for example:-class,abstract,as,async,await,break,
   case,catch,continue,default,deferred,do,dynamic,else,
   enum,export,extends,extension,external,factory,false etc..
+  but a walkaround is to use _ before the reserved keyword or 
+  change the first letter to capital letter if lowercase and 
+  vice versa.
   */
+print('\n--Reserved keyword as variable name--\n');
+  //eg1
+  double Int = 34.6;
+  print(Int);
+  //eg2
+  bool Else = false;
+  print(Else);
+  //eg3
+  int Class = 89;
+  print(Class);
+  //eg4x
+  String If = 'Capt.Jehan';
+  print(If);
 }

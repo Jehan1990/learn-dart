56f8fefcf0b861e255a037a9380e066b2f8ce807
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-18 05:49:40 +0000

    add

---
+++ basic/operator.dart
@@ -0,0 +1,38 @@
+//Operators
+
+void main() {
+  //Arithmetic Operators
+  print('\n--Arithmetic Operators--');
+
+  print(5 + 2); //Addition
+  print(5 - 2); //Subtraction
+  print(5 * 2); //Multiplication
+  print(5 / 2); //Division
+  print(5 ~/ 2); //Division returning integer value
+  print(5 % 2); //Modulus
+
+  //Aguemented Assignment Operators
+  print('\n--Aguemented Assignment Operators--');
+
+  int x = 5;
+  x += 2; //7
+  print(x);
+
+  x = 5;
+  x -= 2; //3
+  print(x);
+
+  x = 5;
+  x *= 2; //10
+  print(x);
+
+  x = 5;
+  x ~/= 2; //x=x/2
+  print(x);
+
+  /*  x /= 2; is not possible because x is 
+      int and result will be double*/
+  double y = 5;
+  y /= 2;
+  print('(5 /= 2)=$y');
+}

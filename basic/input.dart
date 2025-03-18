0a6eb8245cb1d38fc9980bef3ae45854b87a61d5
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-18 17:27:24 +0000

    add

---
+++ basic/input.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+//dart io library is used to take input from user. 
+
+void main() {
+  print('Enter your name');
+  /* stdin.readLineSync() is used to take input from user 
+  which may be null*/
+  String name = stdin.readLineSync()!;
+  //! is used make it non nullable = force non nullable
+  print('Name you have entered is: ${name.toUpperCase()}');
+}
+
+//change setting run in terminal code runner

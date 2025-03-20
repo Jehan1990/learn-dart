e3c132f78ec0b3954be177e7ef2b407f7a3763d5
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-20 17:56:47 +0000

    add

--- operators/logical_relation.dart
+++ operators/logical_relation.dart
@@ -1,16 +1,24 @@
 void main() {
   print('\nRelational Operators\n');
   //Relational Operators >, <, >=, <=, ==, !=
-  print(8 == 2);//equal to
-  print(8 != 2);//not equal to
-  print(8 > 2);//greater than
-  print(8 < 2);//less than
-  print(8 >= 2);//greater than or equal to
-  print(8 <= 2);//less than or equal to
+  print(8 == 2); //equal to
+  print(8 != 2); //not equal to
+  print(8 > 2); //greater than
+  print(8 < 2); //less than
+  print(8 >= 2); //greater than or equal to
+  print(8 <= 2); //less than or equal to
 
   print('\nLogical Operators\n');
   //Logical Operators &&, ||, !
-  print(8 == 2 && 8 != 2);//AND=both have to be true
-  print(8 == 2 || 8 != 2);//OR=any one have to be true
-  print(!(8 == 2));//(!) NOT=convertes true to false and false to true
+  print(8 == 2 && 8 != 2); //AND=both have to be true
+  print(8 == 2 || 8 != 2); //OR=any one have to be true
+  print(!(8 == 2)); //(!) NOT=convertes true to false and false to true
+
+  //Example
+  print('\nExample\n');
+
+  String email = 'jehan.ichhaporia@goindigo.in';
+
+  print(email.isEmpty && email.contains('@'));
+
 }
---
+++ operators/tennary.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 34;
+  String type = age > 18 ? 'Adult' : 'Child';
+  print(type);
+
+  //Tenary operator
+
+  /*condition ? expr1 : expr2
+  If condition is true, expr1 is evaluated and returned,
+  otherwise expr2 is evaluated and returned */
+}

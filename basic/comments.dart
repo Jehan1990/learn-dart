dbfc286568ade21bcfd071db685caf117d385abf
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-21 09:42:55 +0000

    add

---
+++ basic/comments.dart
@@ -0,0 +1,15 @@
+void main(){
+  print('This is a single line comment');
+  // This is a single line comment
+  
+  print('This is a multi line comment');
+  /*
+  This is a multi line comment,
+  This is a multi line comment,
+  This is a multi line comment.
+  */
+
+  print('This is a documentation comment');
+  /// This is used when you want to include this comment in documention.
+
+}
---
+++ basic/expression_statements.dart
@@ -0,0 +1,12 @@
+/// Expression vs Statements
+///Expression-holds a value at runtime
+///Statements-Does not hold a value at runtime
+
+void main() {
+
+  //Expression
+  int x = (10 + 3) * 4;
+
+  //Statement
+  print(x);
+}

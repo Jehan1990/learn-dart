78ea86a65c4ac22835ae2e4df8b2dc84d706db0d
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-23 02:37:25 +0530

    Update excercise_var_final_const.dart

--- exercises/excercise_var_final_const.dart
+++ exercises/excercise_var_final_const.dart
@@ -3,7 +3,7 @@ void main(){
 const text='I love pizza';
 const topping='with tomatoes';
 var favourite = '$text $topping';
-final newText=favourite.replaceAll('pizza','pasta);
+final newText=favourite.replaceAll('pizza','pasta');
 favourite = 'i like curry now';
 print(newText);
 }

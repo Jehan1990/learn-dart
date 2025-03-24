6e4a0bcfb386e70fc3df749fa7f5bbbb06a1407b
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-24 04:53:17 +0000

    add

---
+++ DartTypeSystem/dynamicKeyword.dart
@@ -0,0 +1,20 @@
+/// Dynamic keyword
+/// 
+/// Dynamic keyword is used to get out of dart's type safety 
+/// so if we use dynamic then the type of the variable can be changed.
+
+
+void main(){
+  var name='Jehan';
+  /*name=10;  will create a compilertime error 
+  as we are changing type of name*/
+  dynamic text='ichhaporia';
+  print('String $text');
+  text=34;
+  print('Int $text');
+  text=4.7895;
+  print('Double $text');
+  text=true;
+  print('bool $text');
+
+}

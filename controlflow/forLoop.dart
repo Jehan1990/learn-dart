6657d4d27aa5b85d1a50d9f449ae646ca6cb1303
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-28 06:01:58 +0000

    add

---
+++ controlflow/forLoop.dart
@@ -0,0 +1,13 @@
+///For Loop
+
+void main(){
+  for(var i=1;i<=5;i++){
+    print(i);
+  }
+  print('---------');
+  for(var i=1;i<=5;i++){
+  //same variable i can be used as i is used inside for only
+    print('X'*i);
+  }
+  print('---------');
+}
---
+++ controlflow/whileLoop.dart
@@ -0,0 +1,15 @@
+///While Loop
+
+void main(){
+  var i=1,j=1;
+  while(i<=5){
+    print(i);
+    i++;
+  }
+  print('----------');
+  while(j<=5){
+    print('X'*j);
+    j++;
+  }
+  print('----------');
+}

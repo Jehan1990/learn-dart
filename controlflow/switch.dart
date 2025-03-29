f1a7f8690cf0da6ca5ba1775f076546baa20a8c5
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-29 09:57:57 +0000

    add

---
+++ controlflow/switch.dart
@@ -0,0 +1,18 @@
+void main(){
+  var position=4;
+
+  switch(position){
+    case  1:
+    print('Gold');
+    break;
+    case  2:
+    print('Silver');
+    break;
+    case  3:
+    print('Bronze');
+    break;
+    default:
+    print('No Medal');
+    break;
+  }
+}

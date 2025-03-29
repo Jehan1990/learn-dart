632e49b86fd7978fc68a637101e1c2c83c6e0af7
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-03-29 12:07:31 +0000

    add

---
+++ exercises/enum_excercise.dart
@@ -0,0 +1,26 @@
+enum Operation{plus,minus,multiply,divide}
+
+
+
+void main(){
+  //const a=4,b=2;//plus or minus
+  //const a=6,b=2;//multiply
+  const a=6,b=3;//Divide
+  const op=Operation.divide;
+
+  switch(op){
+    case Operation.plus:
+    print('$a + $b = ${a+b}');
+    break;
+    case Operation.minus:
+    print('$a - $b = ${a-b}');
+    break;
+    case Operation.multiply:
+    print('$a X $b = ${a*b}');
+    break;
+    case Operation.divide:
+    print('$a / $b = ${a/b}');
+    break;
+  }
+
+}

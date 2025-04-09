c11a9a07a330dcfc11d8ac9db6c4a8c85bb8d097
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-04-09 09:01:56 +0000

    add

---
+++ collections/var_const_final_withList.dart
@@ -0,0 +1,19 @@
+void main(){
+
+  var numbers=[2,6,78,300];
+
+  final numbers1=[9,8,7,4];
+
+  const number2=[200,300,400,500];
+
+  numbers[2]=89;//Valid code as numbers is var
+  numbers1[2]=97;//Valid even if declared as final
+  //number2[2]=800;
+  //valid at comilerTime but invalid at runTime as declared as const
+
+print(numbers);
+print(numbers1);
+print(number2);
+
+
+}

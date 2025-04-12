71c0e51be199ef807cd10c877c281859c5b99793
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-04-12 04:36:58 +0000

    add

---
+++ collections/maps_declare.dart
@@ -0,0 +1,25 @@
+///Maps
+///Maps is also called Dictionary in other languages
+///Maps is key - value pair
+
+void main() {
+  //1 declare map
+  Map<String, dynamic> person = {'name': 'Jehan', 'age': 34, 'height': 5.8};
+
+  print('${person['name']} ${person['age']} ${'height'}');
+
+  //2 declare map
+  var car = <String, dynamic>{'make': 'Maruti', 'year': 2022};
+
+  print('${car['make']} ${car['year']}');
+
+  //3 declare map
+  Map car2 = {'length': 4.6, 'year': 2002};
+  
+  print(car2);
+  //4 declare map
+
+  var person1 = {'age': 56, 'height': 5.8, 'hairColor': 'black'};
+
+print(person1);
+}

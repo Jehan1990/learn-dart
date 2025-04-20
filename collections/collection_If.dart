4d8ebe415bf736697e63cdd04808d63f1e404a1a
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-04-20 23:18:31 +0530

    add

--- collections/collection_If.dart
+++ collections/collection_If.dart
@@ -1,13 +1,12 @@
 ///Collection-if:- if statement inside Collections like list,set,map
 
-// ignore_for_file: dead_code
-
 void main() {
   const addBlue = false;
   const addRed = true;
 
   //conventional method
   final colors = ['green', 'grey'];
+  // ignore: dead_code
   if (addBlue) {
     colors.add('blue');
   }
@@ -15,9 +14,15 @@ void main() {
     colors.add('red');
   }
   print(colors);
-
   //collection if method
-  final colors2 = ['green', 'grey', if (addBlue) 'blue', if (addRed) 'red'];
-
+  // ignore: dead_code
+  final colors2 = [
+    'green',
+    'grey',
+    if (addBlue)
+      // ignore: dead_code
+      'blue',
+    if (addRed) 'red',
+  ];
   print(colors2);
 }

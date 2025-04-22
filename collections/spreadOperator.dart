c4b1302ae7d977eefd97972f3150a566f6aeed2d
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-04-23 02:22:26 +0530

    add

--- collections/spreadOperator.dart
+++ collections/spreadOperator.dart
@@ -2,38 +2,29 @@
 ///is used to add all list items in another parent list.
 
 void main() {
-   final parenList = ['Jehan', 'Binu'];
-  final addExtracolors=true;
+  final parenList = ['Jehan', 'Binu'];
+  final addExtracolors = true;
   final extralist = ['Zia', 'Hoshang'];
   final editedList = ['Jehan', 'Binu', extralist];
-  
+
   print('\n----Without Spread Operator----');
   print(editedList);
-  
+
   print('\n----With Spread Operator----');
-  final editedList1 = [
-    'Jehan', 
-    'Binu',
-    ...extralist
-    ];
+  final editedList1 = ['Jehan', 'Binu', ...extralist];
   print(editedList1);
 
   //Collection-if,collection-for,spread are composable
   print('\n---Composable---');
 
-  final editedList2=[
-    'Jehan', 
-    'Binu',
-    if(addExtracolors)
-    ...extralist
-  ];
+  final editedList2 = ['Jehan', 'Binu', if (addExtracolors) ...extralist];
   print(editedList2);
 
   //Another Way to use spread Opearator
   print('\n---Spread Operator 2---');
 
-   final editedList3=[
-    'Jehan', 
+  final editedList3 = [
+    'Jehan',
     'Binu',
     ...['Zia', 'Hoshang'],
   ];
@@ -44,34 +35,30 @@ void main() {
 
   //Using collecton with maps
 
-  print('\n--Using collecton if with Map--');
+  print('\n--Using collecton-if with Map--');
 
-  const rating=[2.3,4.5,4.8,4.9];
-  final restaurants={
-    'name':'Gloria',
-    'cuisine':'Muglai',
-    if(rating.length>3)
-    'rating':rating
+  const rating = [2.3, 4.5, 4.8, 4.9];
+  final restaurants = {
+    'name': 'Gloria',
+    'cuisine': 'Muglai',
+    if (rating.length > 3) 'rating': rating,
   };
 
-print(restaurants);
+  print(restaurants);
   //Using spread if with maps
 
-  print('\n--Using spraed with Map--');
+  print('\n--Using spread with Map--');
 
-  const rating2=[2.3,4.5,4.8,4.9];
-  final restaurants1={
-    'name':'Gloria',
-    'cuisine':'Muglai',
-    if(rating.length>3)...{//spread and if
-    'rating':rating,
-    'isPolular':true
-    }
+  const rating2 = [2.3, 4.5, 4.8, 4.9];
+  final restaurants1 = {
+    'name': 'Gloria',
+    'cuisine': 'Muglai',
+    if (rating.length > 3) ...{
+      //spread and if
+      'rating': rating,
+      'isPolular': true,
+    },
   };
 
-print(restaurants1);
-
-
-
-
+  print(restaurants1);
 }

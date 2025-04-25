ef52d7e4077ede00ad24013ac69b700f4fd9e98c
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-04-25 15:09:13 +0530

    add

---
+++ projects/data_processing/app.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  /*the main method in dart can take List<String> only in 
+   CLA(Command Line App) 
+   Meaning of (List<String> arguments) is :-
+   1.DataType of parameters is List<String>
+   2.Variable name of parameter is arguments
+   */
+  if (arguments.isEmpty) {
+    print('Usage:dart app.dart<inputFile.csv>');
+    exit(1); //0 is when no error,
+    //non zero is when there is an error. exit is from dart:io library
+  }
+  final inputFile = arguments.first;
+  print(inputFile);
+}

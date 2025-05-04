a975b7292db8918c6797ceff372fbf530ae27f22
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-05-04 23:51:25 +0530

    add

--- null_nullSafety/if_null.dart
+++ null_nullSafety/if_null.dart
@@ -1,8 +1,32 @@
-/// if-null operator ??
+/// if-null operator (??)
 
 void main() {
   int? x;
 
-  //using tenary operator
+  //x = 5;
+
+  //using tenary operator for dealing with null condition
   int y = x == null ? 0 : x;
+  print(y);
+
+  //using if-null operator (??)
+  int z = x ?? 0;
+  print(z);
+
+  //Agumented assignment if-null operator
+
+  x ??= 0; //Agumented assignment if-null operator
+  int k = x;
+  print(k);
+
+  /*Revision Null:-
+  -A variable can be null.
+  -there are nullable and non-nullable variable.
+  -data types are non-nullable (due to null safety).
+  -data type can be made by using ?.
+  -var,final,const can be null with type inference.
+  -Assertion operator ! is used when you are sure at runtime variable 
+  will not be null. Its also called bang operator.
+  -if-null operator is ??.
+   */
 }

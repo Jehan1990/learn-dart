0bccd6701407b21f3a411547ce4a8574b8ee2e5c
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-09-09 15:40:21 +0000

    add

---
+++ functions/basicFuction.dart
@@ -0,0 +1,11 @@
+//Functions are the building blocks 
+//You can save a type of code and reuse it.
+
+void main(){
+  greeting();
+  greeting();//using the fuction again
+}
+
+void greeting(){
+  print('Hello Welcome to function');
+}

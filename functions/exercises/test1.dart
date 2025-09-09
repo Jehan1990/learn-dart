d6750c31718cbf19151c6aa3a3899d49a67ec8cc
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-09-09 16:46:19 +0000

    add

--- functions/exercises/test1.dart
+++ functions/exercises/test1.dart
@@ -1,7 +1,9 @@
 void main(){
-  print(sum([]));
-  print(sum([1,2,3]));
-  print(sum([1.5,2.58,3.5]));
+  print(sum([]));//empty list
+  print(sum([1,2,3]));//integer list
+  print(sum([1.5678,2.58112,4.5344]));//double list
+  var total=sum([4,8.0,9.0]);
+  print(total);//different way of printing
 }
 
 double sum(List<double> values){

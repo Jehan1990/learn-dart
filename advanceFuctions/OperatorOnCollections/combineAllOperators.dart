e7234eedcd70093a079cee6a7928e0a53ed8c04c
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-09-26 02:53:00 +0000

    add

--- advanceFuctions/OperatorOnCollections/combineAllOperators.dart
+++ advanceFuctions/OperatorOnCollections/combineAllOperators.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+
 
 ///Combine all operators
 
--- advanceFuctions/excersie1.dart
+++ advanceFuctions/excersie1.dart
@@ -4,11 +4,13 @@ void main() {
   print(ans);
 }
 
-T firstWhere<T>(List<T> items, bool Function(T) f, {required T Function() orElse}) {
+T firstWhere<T>(List<T> items, bool Function(T) f, { T Function()? orElse}) {
   for (var item in items) {
     if (f(item)) {
       return item;
     }
   }
-  return orElse();
+  return orElse!();
+  //using null assersition operator ! to confirm it will not be null
+
 }

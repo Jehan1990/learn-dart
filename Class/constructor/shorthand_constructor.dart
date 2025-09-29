fb3121664ceb8655151a717848693c22b272d0de
Jehan1990 <jehan.ichhaporia.ji@gmail.com>
2025-09-29 14:37:53 +0000

    add

--- Class/constructor/shorthand_constructor.dart
+++ Class/constructor/shorthand_constructor.dart
@@ -1,12 +1,12 @@
 ///ShortHand constructor - is different way to create the constructor
+///constructors are declared before properties and methods in class by convention.
 
 class BankAccount {
-  double balance;
-  String accountHolder;
-
   //Shorthand constructor- different way to create a constructor
   BankAccount({this.balance = 0, required this.accountHolder});
-
+  
+  double balance;
+  String accountHolder;
   void deposit(double amount) {
     //method of the class
     balance += amount;
